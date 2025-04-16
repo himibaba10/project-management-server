932b67368c1d918786c0a6bb62ea4d4cca907a4f
Ferdous Ahmed <himibaba10@gmail.com>
2025-04-16 18:25:59 +0600

    Added helmet for security

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,15 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import userRouter from "./app/routes/user.route";
 import cookieParser from "cookie-parser";
 import projectRouter from "./app/routes/project.route";
-import limiter from "./app/middlewares/limiter";
+import limiter from "./app/config/limiter";
+import helmet from "helmet";
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(limiter);
+app.use(helmet());
 
 // Routes
 app.get("/", (req, res) => {
